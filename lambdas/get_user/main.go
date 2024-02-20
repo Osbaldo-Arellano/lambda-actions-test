595da43979f4c61baa17f1c710d04867747aab07
Osbaldo-Arellano <osbaldoarellano1996@gmail.com>
2024-02-20 15:57:14 -0800

    asdas

--- lambdas/get_user/main.go
+++ lambdas/get_user/main.go
@@ -12,7 +12,7 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	sourceIP := request.RequestContext.Identity.SourceIP
 
 	if sourceIP == "" {
-		fmt.Println("tahis is sneawass from the Usdzaasdasdsdfsssdfsddser lambdassdfsdfsddasda!asdasd!!!!asdfa!")
+		fmt.Println("tahis is sneawaaass from the Usdzaasdasdsdfsssdfsddser lambdassdfsdfsddasda!asdasd!!!!asdfa!")
 
 		greeting = "Hadasello, world!\n"
 	} else {
